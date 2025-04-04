341841faafecb7a94f556b57c59e95407ed34fe5
Vivek Chaudhary <51666451+ichaudharyvivek@users.noreply.github.com>
2025-04-04 19:33:18 +0000

    Done.

--- cmd/api/router/router.go
+++ cmd/api/router/router.go
@@ -5,15 +5,26 @@ import (
 
 	"example.com/api-go/cmd/api/resource/book"
 	"example.com/api-go/cmd/api/resource/health"
+	_ "example.com/api-go/docs"
+	"github.com/go-chi/cors"
 	"github.com/go-playground/validator/v10"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/gorm"
 )
 
 func New(db *gorm.DB, v *validator.Validate) *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
 	r.Get("/health", health.Read)
-
+	r.Get("/swagger/*", httpSwagger.WrapHandler)
 	r.Route("/v1", func(r chi.Router) {
 		bookAPI := book.New(db, v)
 		r.Get("/books", bookAPI.List)
